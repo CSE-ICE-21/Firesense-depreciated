fbc20cf2a082ae054847a0ac67c6709dbb53738e
chathura-de-silva <ac.chathura@gmail.com>
2024-09-29 14:05:27 +0530

    Code refactor for Node

--- Node/include/functions.h
+++ Node/include/functions.h
@@ -2,8 +2,5 @@
 //utils
 void setupSerial();
 void initLora();
-void loraCallBacksRegister();
 
 // lora
-void onCadDone(boolean signalDetected);
-void onReceive(int packetSize);
--- Node/src/main.cpp
+++ Node/src/main.cpp
@@ -4,13 +4,6 @@
 void setup() {
   setupSerial();
   initLora();
-  // LoRa.onTxDone(onTxDone);
-  // LoRa.onReceive(onReceive);
-  LoRa.onCadDone(onCadDone);
-  LoRa.onReceive(onReceive);
-   // put the radio into CAD mode
-   LoRa.channelActivityDetection();
-  
 }
 
 void loop() {
