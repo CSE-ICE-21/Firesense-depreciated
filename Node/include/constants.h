e648887c58f69927235c00e26140e555231189ed
chathura-de-silva <ac.chathura@gmail.com>
2024-10-03 02:00:56 +0530

    Pin NUmber for Lora Module reset changed.

--- Node/include/constants.h
+++ Node/include/constants.h
@@ -1,7 +1,6 @@
-//define the pins used by the lora transceiver module
-
+//define the pins used by the lora transceiver module and sensors
 #define SS 5
-#define RST 15
+#define RST 3
 #define DIO0 GPIO_NUM_2 // Dont define just with numbers, use GPIO_NUM_X
 #define SENSOR_INPUT GPIO_NUM_13 // Dont define just with numbers, use GPIO_NUM_X
 
