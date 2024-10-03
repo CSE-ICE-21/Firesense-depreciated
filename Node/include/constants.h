1cf3328bff7b8c3f9eeb25b54006aa18acbd3312
chathura-de-silva <ac.chathura@gmail.com>
2024-10-03 23:07:42 +0530

    GPIO Change due to serial conflict

--- Node/include/constants.h
+++ Node/include/constants.h
@@ -1,7 +1,7 @@
 //define the pins used by the lora transceiver module and sensors
 #define SS 5
-#define RST 3
+#define RST 22
 #define DIO0 GPIO_NUM_2 // Dont define just with numbers, use GPIO_NUM_X
-#define SENSOR_INPUT GPIO_NUM_13 // Dont define just with numbers, use GPIO_NUM_X
+#define SENSOR_INPUT GPIO_NUM_15 // Dont define just with numbers, use GPIO_NUM_X
 
 #define LISTENING_INTERVAL 5000
